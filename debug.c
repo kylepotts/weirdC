7a641da3befaa91295c35899eaf5b788bf825d39
kylepotts <kyle@kptechblog.com>
2015-03-12 17:21:21 -0400

    update debug

--- debug.c
+++ debug.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-#define HERE    fprintf(stderr, "[LOG]%s:%d\n",__FILE__,__LINE__);
+#define HERE    fprintf(stderr, "[LOG]%s:%d:%s\n:",__FILE__,__LINE__,__func__);
+
+int fib(int n){
+    HERE
+    if(n==0){
+        return 1;
+    }else{
+        return fib(n-1);
+    }
+}
+
+
 
 int main(){
     int a = 1;
     HERE
+    int b = fib(4);
 }
